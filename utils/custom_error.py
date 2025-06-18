6bd0f980aa372658ab9e1a4fc197e16117ee9aac
DyuthiVivek <dyuthi.vivek@gmail.com>
2025-06-18 16:36:01 +0530

    refactor

--- utils/custom_error.py
+++
@@ -1,5 +0,0 @@
-class CustomError(Exception):
-    def __init__(self, message, status_code=500):
-        self.message = message
-        self.status_code = status_code
-        super().__init__(self.message)
